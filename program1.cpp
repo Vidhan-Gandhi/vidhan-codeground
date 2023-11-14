905c5dc8297dfeb15880f9dc2a1f289b9da29edb
Vidhan <vgbtech2327@gmail.com>
2023-11-14 20:37:39 +0530

    First commit

---
+++ program1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    // Your code here
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << "sum = " << num1 + num2;
+
+    return 0;
+}
